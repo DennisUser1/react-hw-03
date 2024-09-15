9a7900a5e5e31c70f59365c7f396fbf7112eafad
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2024-09-15 21:48:25 +0300

    Initial Commit. Added ContactList + configured package.json

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react";
+import ContactForm from "./ContactForm";
+import SearchBox from "./SearchBox";
+import ContactList from "./ContactList";
+import initialContacts from './db/contacts.json';
+import { FaAddressBook } from "react-icons/fa";
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,20 @@
+import styles from "./ContactList.module.css";
+
+export default function ContactList({ contacts, deleteContact }) {
+    return (
+      <ul className={styles.contactsList}>
+        {contacts.map((contact) => {
+          return (
+            <li className={styles.contactItem} key={contact.id}>
+              <Contact
+                id={contact.id}
+                name={contact.name}
+                number={contact.number}
+                deleteContact={deleteContact}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
