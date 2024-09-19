bbbfc17d7d472b28dbfc07df39c17b7f32ec2d24
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2024-09-19 12:41:53 +0300

    v.1.9. Fixed bugs

--- src/helpers/contactSchema.js
+++ src/helpers/contactSchema.js
@@ -3,13 +3,13 @@ import * as Yup from 'yup';
 export const validationContactSchema = Yup.object().shape({
   name: Yup.string()
     .trim()
-    .matches(/^[A-Za-z\s-]+$/, "Must be a valid name! Name must contain only letters, spaces, or hyphens")
+    .matches(/^[A-Za-z\s-]+$/, "Invalid name field symbols! Please, use only letters, spaces, or hyphens")
     .min(3, 'Too Short Name! Minimum characters: 3')
     .max(28, 'Too Long Name! Maximum characters: 28')
     .required('Here is a required field'),
   number: Yup.string()
     .trim()
-    .matches(/^[0-9+\-()\s]*$/, "Invalid symbols phone number! Please, use only digits and symbols ( + or - ( or ))")
+    .matches(/^[0-9+\-()\s]*$/, "Invalid symbols phone number! Please, use only digits and symbols. Example: + or - or ( or )")
     .min(7, 'Too Short Number! Minimum characters: 7')
     .max(15, 'Too Long Number! Maximum characters: 15')
     .required('Here is a required field'),
